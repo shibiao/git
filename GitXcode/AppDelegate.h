4290072685677c03736d1b597660cf30e7b37c73
shibiao <956035125@qq.com>
2016-07-04 19:19:22 +0800

    Revert "1"
    This reverts commit 201c18e2c85b5c60d495e80dd5d45772be17a7f0.

---
+++ GitXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitXcode
+//
+//  Created by Mac on 16/7/1.
+//  Copyright © 2016年 shibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitXcode
+//
+//  Created by Mac on 16/7/1.
+//  Copyright © 2016年 shibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
